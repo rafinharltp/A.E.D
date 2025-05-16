caa256e66d1b509c090ad8f0468c849e400f71ba
Rafael Persiguelo <rafinhapersiguelo@gmail.com>
2025-05-15 22:07:37 -0300

    Add files via upload

---
+++ Nó/No.java
@@ -0,0 +1,25 @@
+public class No {
+    int valor;
+    No proximo;
+
+    public No(int valor, No proximo) {
+        this.valor = valor;
+        this.proximo = proximo;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public No getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
+}
