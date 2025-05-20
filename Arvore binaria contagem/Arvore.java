1b9b64cdf3c85dc1b740fb2a53d34e466447204c
Rafael Persiguelo <rafinhapersiguelo@gmail.com>
2025-05-20 20:01:59 -0300

    Add files via upload

---
+++ Arvore binaria contagem/Arvore.java
@@ -0,0 +1,28 @@
+public class Arvore {
+
+    public int contarNos(No node) {
+        if (node == null) return 0;
+        return 1 + contarNos(node.esquerda) + contarNos(node.direita);
+    }
+
+    public static void main(String[] args) {
+
+        No a = new No("A");
+        No b = new No("B");
+        No c = new No("C");
+        No d = new No("D");
+        No e = new No("E");
+        No f = new No("F");
+
+        a.esquerda = b;
+        a.direita = c;
+        b.esquerda = d;
+        b.direita = e;
+        c.direita = f;
+
+        Arvore arvore = new Arvore();
+        int totalNos = arvore.contarNos(a);
+
+        System.out.println("Total de nos na arvore: " + totalNos);
+    }
+}
---
+++ Arvore binaria contagem/No.java
@@ -0,0 +1,11 @@
+class No {
+    String valor;
+    No esquerda;
+    No direita;
+
+    No(String valor) {
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+}
